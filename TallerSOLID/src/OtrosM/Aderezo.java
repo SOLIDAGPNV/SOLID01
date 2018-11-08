306446b8024311ad8676d0739bb56c55797bf7f6
Administrador <Administrador@GGIECUT1WRK015.espol.edu.ec>
2018-11-08 12:02:10 -0500

    Adición de carpetas

---
+++ TallerSOLID/src/OtrosM/Aderezo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OtrosM;
+
+import Otros.*;
+
+/**
+ *
+ * @author Pedro Mendoza
+ */
+public enum Aderezo {
+    MALVAVISCOS,GRAGEAS,CHISPAS,FRUTILLA,CREMA
+}
